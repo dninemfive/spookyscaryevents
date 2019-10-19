8305a4a14a9662c8ccbb8a02b344af03f6c1358c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-10-19 13:01:17 -0700

    gonna change how the conditions work

--- Source/Halloween/GameComponent_HalloweenTracker.cs
+++ Source/Halloween/GameComponent_HalloweenTracker.cs
@@ -9,14 +9,39 @@ namespace D9Halloween
 {
     class GameComponent_HalloweenTracker : GameComponent
     {
+        Game game;
         Dictionary<long, IncidentInfo> incidents;
-        GameComponent_HalloweenTracker(Game game)
+
+        public Dictionary<float, Dictionary<int, float>> SunriseTimes, SunsetTimes;
+        GameComponent_HalloweenTracker(Game g)
         {
+            game = g;
+            GetSunriseAndSunsetTimes();
+        }
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            //if loading and mod list hash != saved hash, GetSunriseAndSunsetTimes();
         }
         public override void GameComponentTick()
         {
             base.GameComponentTick();
+        }
+        private void GetSunriseAndSunsetTimes()
+        {
+            SunriseTimes = new Dictionary<float, Dictionary<int, float>>();
+            foreach(Map map in game.Maps)
+            {
+                float lat = Find.WorldGrid.LongLatOf(map.Tile).x;
+                if (!SunriseTimes.ContainsKey(lat)) {
+                    Dictionary<int, float> temp = new Dictionary<int, float>();
+                    for (int t = 0; t < GenDate.TicksPerYear; t++)
+                    {
+                        int last0 = 0;
 
+                    }
+                }
+            }
         }
     }
 }
