862725d8d1daf60449ce0a67f57ccbd03b080232
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-10-21 19:26:23 -0700

    add some defs, start firestarter work

---
+++ Source/Halloween/CompFireStarter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace D9Halloween
+{
+    class CompFireStarter : ThingCompWithCheapHashInterval
+    {
+        public override int TickInterval => 20;
+    }
+}
